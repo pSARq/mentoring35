55c919754b2802b8b941d831c6d857e78c08dc8c
Santiago Almendra <santiagoalmendrarojas19@gmail.com>
2022-01-10 16:15:19 -0500

    Se agrego la función de delete

--- back/src/main/java/co/com/sofka/mentoring35/usecases/UseCaseDelete.java
+++ back/src/main/java/co/com/sofka/mentoring35/usecases/UseCaseDelete.java
@@ -3,12 +3,16 @@ package co.com.sofka.mentoring35.usecases;
 import co.com.sofka.mentoring35.mappers.Mapper;
 import co.com.sofka.mentoring35.repositories.RandomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.internal.Function;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
 
 @Service
 @Validated
-public class UseCaseDelete {
+public class UseCaseDelete implements Function<String, Mono<Void>> {
 
     private final RandomRepository randomRepository;
     private final Mapper mapper;
@@ -17,5 +21,12 @@ public class UseCaseDelete {
     public UseCaseDelete(RandomRepository randomRepository, Mapper mapper) {
         this.randomRepository = randomRepository;
         this.mapper = mapper;
+    }
+
+
+    @Override
+    public Mono<Void> apply(String id) {
+        Objects.requireNonNull(id, "El id no puede ser nulo");
+        return randomRepository.deleteById(id);
     }
 }
