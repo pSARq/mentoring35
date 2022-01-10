893313990a07751b72b2b4746d617234930924e9
Santiago Almendra <santiagoalmendrarojas19@gmail.com>
2022-01-10 16:09:10 -0500

    Se agrega funcion de getById

---
+++ back/src/main/java/co/com/sofka/mentoring35/routers/RouterGetById.java
@@ -0,0 +1,29 @@
+package co.com.sofka.mentoring35.routers;
+
+import co.com.sofka.mentoring35.models.RequestDTO;
+import co.com.sofka.mentoring35.usecases.UseCaseGetById;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+public class RouterGetById {
+
+    @Bean
+    public RouterFunction<ServerResponse> getById(UseCaseGetById useCaseGetById){
+        return route(GET("/get/{id}").and(accept(MediaType.APPLICATION_JSON)),
+                request -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(BodyInserters.fromPublisher(useCaseGetById.apply(
+                                        request.pathVariable("id")), RequestDTO.class
+                        )).onErrorResume(throwable -> ServerResponse.badRequest().body(throwable.getMessage(), String.class))
+        );
+    }
+}
