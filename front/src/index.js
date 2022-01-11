35ba01e771bbdd412ffd08a2829892df19c06d91
Santiago Almendra <santiagoalmendrarojas19@gmail.com>
2022-01-11 11:29:09 -0500

    Se corrigio el middleware del store en index.js

--- front/src/index.js
+++ front/src/index.js
@@ -3,19 +3,13 @@ import { render } from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
-import { createLogger } from 'redux-logger'
-import reducer from './reducers'
 import App from './containers/App'
 import { composeWithDevTools } from 'redux-devtools-extension'
-
-const middleware = [ thunk ]
-if (process.env.NODE_ENV !== 'production') {
-  middleware.push(createLogger())
-}
+import randomReducer from './reducers'
 
 const store = createStore(
-  reducer,
-  composeWithDevTools(applyMiddleware(...middleware))
+  randomReducer,
+  composeWithDevTools(applyMiddleware(thunk))
 )
 
 render(
